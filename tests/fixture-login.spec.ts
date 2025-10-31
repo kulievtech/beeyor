750a86f49a3fed464a156006094d98c2a412a088
Tohir Kuliev <kuliev.tech@gmail.com>
2025-10-30 21:58:55 -0400

    Add tags

--- tests/fixture-login.spec.ts
+++ tests/fixture-login.spec.ts
@@ -1,8 +1,12 @@
 import { expect } from "@playwright/test";
 import { test } from "fixtures/auth";
 
-test("User can see profile", async ({ myAccountPage, user }) => {
-  const logOutText = await myAccountPage.getLogOutText();
-  console.log(`Logged in user is: ${user.username}`);
-  expect(logOutText).toBe("Log out");
-});
+test(
+  "User can see profile",
+  { tag: ["@smoke"] },
+  async ({ myAccountPage, user }) => {
+    const logOutText = await myAccountPage.getLogOutText();
+    console.log(`Logged in user is: ${user.username}`);
+    expect(logOutText).toBe("Log out");
+  }
+);
--- tests/helper-login.spec.ts
+++ tests/helper-login.spec.ts
@@ -1,8 +1,12 @@
 import test, { expect } from "@playwright/test";
 import { login } from "helpers/login";
 
-test("Helper - Verify a user can login", async ({ page }) => {
-  const accountPage = await login(page);
-  const logOutText = await accountPage.getLogOutText();
-  expect(logOutText).toBe("Log out");
-});
+test(
+  "Helper - Verify a user can login",
+  { tag: ["@smoke"] },
+  async ({ page }) => {
+    const accountPage = await login(page);
+    const logOutText = await accountPage.getLogOutText();
+    expect(logOutText).toBe("Log out");
+  }
+);
