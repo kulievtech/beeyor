34a64a0406b36c0daf0df66ad48e0055cfff18d8
Tohir Kuliev <kuliev.tech@gmail.com>
2025-12-31 15:27:28 -0500

    Update

--- tests/shop/product-title-price.spec.ts
+++ tests/shop/product-title-price.spec.ts
@@ -3,51 +3,55 @@ import { expect } from "@playwright/test";
 import { goToShopPage } from "actions/navigation";
 import { test } from "fixtures/auth";
 
-test.describe("Products Present on Shop Page", { tag: ["@smoke"] }, () => {
-  let shopPage: ShopPage;
+test.describe(
+  "Products Title and Price Present on the Shop Page",
+  { tag: ["@smoke"] },
+  () => {
+    let shopPage: ShopPage;
 
-  test.beforeEach(async ({ page }) => {
-    // 1. Go to Shop Page
-    shopPage = await goToShopPage(page);
+    test.beforeEach(async ({ page }) => {
+      // 1. Go to Shop Page
+      shopPage = await goToShopPage(page);
 
-    // 2. Wait until page is loaded
-    await shopPage.waitUntilPageIsLoaded();
-  });
+      // 2. Wait until page is loaded
+      await shopPage.waitUntilPageIsLoaded();
+    });
 
-  test("Trending Products have valid titles and prices", async () => {
-    // 3. Get Trending Products table and current products
-    const productsTable = shopPage.getTrendingProductsTable();
-    const products = await productsTable.getCurrentProducts();
+    test("Trending Products have valid titles and prices", async () => {
+      // 3. Get Trending Products table and current products
+      const productsTable = shopPage.getTrendingProductsTable();
+      const products = await productsTable.getCurrentProducts();
 
-    // 4. Verify that each product has a valid title and price
-    for (const product of products) {
-      const title = await product.getTitle();
-      const price = await product.getPrice();
-      // If product does not have a price, it returns 0
-      if (price === 0) continue;
+      // 4. Verify that each product has a valid title and price
+      for (const product of products) {
+        const title = await product.getTitle();
+        const price = await product.getPrice();
+        // If product does not have a price, it returns 0
+        if (price === 0) continue;
 
-      expect.soft(title.length).toBeGreaterThan(3);
-      expect.soft(price).toBeGreaterThan(0);
-    }
-  });
+        expect.soft(title.length).toBeGreaterThan(3);
+        expect.soft(price).toBeGreaterThan(0);
+      }
+    });
 
-  test("New Arrivals Products have valid titles and prices", async () => {
-    // 3. Get New Arrivals Products table and current products
-    const productsTable = shopPage.getNewArrivalsProductsTable();
-    const products = await productsTable.getCurrentProducts();
+    test("New Arrivals Products have valid titles and prices", async () => {
+      // 3. Get New Arrivals Products table and current products
+      const productsTable = shopPage.getNewArrivalsProductsTable();
+      const products = await productsTable.getCurrentProducts();
 
-    // 4. Verify that each product has a valid title and price
-    for (const product of products) {
-      const title = await product.getTitle();
-      const price = await product.getPrice();
-      // If product does not have a price, it returns 0
-      if (price === 0) continue;
+      // 4. Verify that each product has a valid title and price
+      for (const product of products) {
+        const title = await product.getTitle();
+        const price = await product.getPrice();
+        // If product does not have a price, it returns 0
+        if (price === 0) continue;
 
-      expect.soft(title.length).toBeGreaterThan(3);
-      expect.soft(price).toBeGreaterThan(0);
-    }
-  });
-});
+        expect.soft(title.length).toBeGreaterThan(3);
+        expect.soft(price).toBeGreaterThan(0);
+      }
+    });
+  }
+);
 
 // const tables = ["Trending", "New Arrivals"];
 
